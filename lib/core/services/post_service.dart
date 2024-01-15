ea30c9d25ed8b3cf51870b9f865dc6ab1e62fec2
IbrahimWad <ibrahim.amude.781@gmail.com>
2024-01-16 00:04:21 +0300

    Edit

---
+++ lib/core/services/post_service.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce/model/product_model.dart';
+import 'package:ecommerce/model/user_model.dart';
+
+class FireStoreProduct {
+  final CollectionReference _userCollectionRef =
+      FirebaseFirestore.instance.collection('Products');
+
+  Future<void> addProductToFireStore(ProductModel productModel) async {
+    return await _userCollectionRef
+        .doc("${productModel.id}")
+        .set(productModel.toJson());
+  }
+}
